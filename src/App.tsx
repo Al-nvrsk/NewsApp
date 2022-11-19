0ce98fb61ad9fdf37072ebce310cf014f5db2bf6
Al <Batuten.mail@gmail.com>
2022-11-19 21:23:21 +0400

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { Suspense } from "react"
+import { Route, Routes, Link } from "react-router-dom"
+import { AboutPageAsync } from "./page/AboutPage/AboutPage.async"
+import { MainPageAsync } from "./page/MainPage/MainPage.asynx"
+
 export const App = () => {
   return (
     <div>
         App
+        <Link to = {'/'}> Main page </Link>
+        <Link to = {'/about'}> About </Link>
+        <Suspense fallback = {<div> Loading... </div>}>
+            <Routes>
+                <Route path = {`/about`} element = {<AboutPageAsync />}/>
+                <Route path = {'/'}  element = {<MainPageAsync />}/>
+            </Routes>
+        </Suspense>
     </div>
   )
 }
