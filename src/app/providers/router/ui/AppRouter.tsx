2003bd68739924955ff0ba38dc43d8b667173fbf
Al <Batuten.mail@gmail.com>
2022-12-25 12:15:12 +0400

    add jest, add first test

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,10 +1,17 @@
 import { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
 import { Routes, Route } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
 
 export function AppRouter() {
+    const { t } = useTranslation('translation');
     return (
-        <Suspense fallback={<div> Loading... </div>}>
+        <Suspense fallback={(
+            <div>
+                {t('Loading...')}
+            </div>
+        )}
+        >
             <Routes>
                 {Object.values(routeConfig).map(({ element, path }) => (
                     <Route
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
+import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -10,6 +11,7 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
+    const { t } = useTranslation('translation');
 
     const onToggle = () => {
         setCollapsed((prev) => !prev);
@@ -17,7 +19,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
 
     return (
         <div className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
-            <button type="button" onClick={onToggle}>toggle</button>
+            <button type="button" onClick={onToggle}>{t('toggle')}</button>
             <div className={cls.swithers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
