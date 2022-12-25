2c8f9b54f7658ec74f5710ef5a8b5cf97f38ea81
Al <Batuten.mail@gmail.com>
2022-12-25 10:38:12 +0400

    add stylelint, i18n plugin for eslint

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import cls from './Navbar.module.scss';
@@ -6,16 +7,19 @@ interface NavbarProps {
     className?: string
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(cls.Navbar, {}, [className])}>
+export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation(['main', 'about']);
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
 
-        <div className={cls.links}>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
-                Main page
-            </AppLink>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
-                About
-            </AppLink>
+            <div className={cls.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
+                    {t('Главная страница')}
+                </AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
+                    {t('О сайте', { ns: 'about' })}
+                </AppLink>
+            </div>
         </div>
-    </div>
-);
+    );
+};
