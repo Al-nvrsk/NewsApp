3fc53ff8e0a6213c6fb9ba16e500022a8802a92a
Al <Batuten.mail@gmail.com>
2022-12-27 23:30:01 +0400

    add react testing library, tests for btn, sidebar

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import {
     DefinePlugin, HotModuleReplacementPlugin, ProgressPlugin, WebpackPluginInstance,
 } from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev }: BuildOptions): WebpackPluginInstance[] {
@@ -20,5 +21,8 @@ export function buildPlugins({ paths, isDev }: BuildOptions): WebpackPluginInsta
 
         }),
         new HotModuleReplacementPlugin(),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }),
     ];
 }
---
+++ src/shared/ui/Button/Button.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import { Button, ThemeButton } from './Button';
+
+describe('Button', () => {
+    test('Button rendered with text Test', () => {
+        // eslint-disable-next-line i18next/no-literal-string
+        render(<Button> test </Button>);
+        expect(screen.getByText('test')).toBeInTheDocument();
+    });
+    test('Button rendered with class clear', () => {
+        // eslint-disable-next-line i18next/no-literal-string
+        render(<Button theme={ThemeButton.CLEAR}> test </Button>);
+        expect(screen.getByText('test')).toHaveClass('clear');
+    });
+});
