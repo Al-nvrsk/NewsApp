8d41d4eaa8d6f4326e22b38b2f5eec3c25dcd102
Al <Batuten.mail@gmail.com>
2023-02-27 16:45:57 +0400

    add fetching articles, view type selector

---
+++ src/page/ArticlesPage/model/slice/articlePageSlice.ts
@@ -0,0 +1,52 @@
+import { createEntityAdapter, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { Article, ArticleView } from 'entities/Article';
+import { ARTICLE_VIEW_LOCALSTORAGE_KEY } from 'shared/const/localStorage';
+import { fetchArticlesPageList } from '../services/fetchArticlesPage/fetchArticlesPage';
+import { ArticlesPageSchema } from '../types/articlesPageSchema';
+
+// Since we don't provide `selectId`, it defaults to assuming `entity.id` is the right field
+const articlesAdapter = createEntityAdapter<Article>({
+    selectId: (article) => article.id,
+});
+
+export const getArticles = articlesAdapter.getSelectors<StateSchema>(
+    (state) => state.articlesPage || articlesAdapter.getInitialState(),
+);
+
+const articlesPageSlice = createSlice({
+    name: 'articleDetailsCommentsSlice',
+    initialState: articlesAdapter.getInitialState<ArticlesPageSchema>({
+        isLoading: false,
+        ids: [],
+        error: undefined,
+        entities: {},
+        view: ArticleView.SMALL,
+    }),
+    reducers: {
+        setView: (state, action: PayloadAction<ArticleView>) => {
+            state.view = action.payload;
+            localStorage.setItem(ARTICLE_VIEW_LOCALSTORAGE_KEY, action.payload);
+        },
+        initState: (state) => {
+            state.view = localStorage.getItem(ARTICLE_VIEW_LOCALSTORAGE_KEY) as ArticleView;
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchArticlesPageList.pending, (state) => {
+                state.error = undefined;
+                state.isLoading = true;
+            })
+            .addCase(fetchArticlesPageList.fulfilled, (state, action: PayloadAction<Article[]>) => {
+                state.isLoading = false;
+                articlesAdapter.setAll(state, action.payload);
+            })
+            .addCase(fetchArticlesPageList.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            });
+    },
+});
+
+export const { reducer: articlesPageReducer, actions: articlesPageActions } = articlesPageSlice;
